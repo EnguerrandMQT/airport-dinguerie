4ad0579d2c1a3e90b42c33ac625cf8caa3682938
EnguerrandMQT <enguerrand.marquant@student.junia.com>
2021-12-07 13:53:21 +0100

    commit

--- src/function.cpp
+++ src/function.cpp
@@ -3,8 +3,10 @@
 
 
 
-int random(int val1, int val2)
+int random(int val1, int val2) 
+
 {
+cout << endl;
     if (val1 > val2)
         swap(val1, val2);
     return (rand() % ((val2 - val1) + 1)) + val1;
